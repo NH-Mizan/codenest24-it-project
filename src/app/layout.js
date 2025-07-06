e1b79b6fc625f2bb17025e57ce72d7237160ae2e
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-07-06 12:26:08 +0600

    Demo  Folder Structure start

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/layouts/Header";
+import Footer from "@/components/layouts/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavBar/>
+        <main>
+           {children}
+        </main>
+        <Footer/>
       </body>
     </html>
   );
