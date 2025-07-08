fc1695e148e85c5ef3a6494b37ffae92b6dc29a8
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-07-08 21:53:40 +0600

    what do me section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Home/Banner";
+import WhatDo from "@/components/Home/whatDoMe";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
      <Banner/>
+     <WhatDo/>
    </div>
   );
 }
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -11,36 +11,31 @@ export default function NavBar() {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
+                            <li><a>Home </a></li>
                             <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
+                                <a>About</a>
                             </li>
-                            <li><a>Item 3</a></li>
+                            <li><a>Services</a></li>
+                            <li><a>Our Team</a></li>
+                            <li><a>Projects</a></li>
+                           
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">Code<span className="text-green-400">Nest</span>24</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                         <li><a>Home </a></li>
+                            <li>
+                                <a>About</a>
+                            </li>
+                            <li><a>Services</a></li>
+                            <li><a>Our Team</a></li>
+                            <li><a>Projects</a></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn bg-green-400">Live Chat</a>
                 </div>
             </div>
         </div>
