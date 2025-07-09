19f4ee6785274ca792fe7a5939443a73e0b5c072
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-07-09 14:59:14 +0600

    start next stup

--- src/components/Home/whatDoMe.jsx
+++ src/components/Home/whatDoMe.jsx
@@ -2,12 +2,9 @@
 export default function WhatDo() {
     return (
         <div>
-            <div className="w-11/12 mx-auto bg-base-200">
-                <div className="flex  lg:flex-row-reverse">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        className="max-w-sm rounded-lg shadow-2xl"
-                    />
+            <div className="w-11/12 mx-auto ">
+                <div className="flex justify-between py-12 items-center">
+                    
                     <div>
                         <h1 className="text-5xl font-bold">Box Office News!</h1>
                         <p className="py-6">
@@ -16,6 +13,10 @@ export default function WhatDo() {
                         </p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                        className="max-w-sm rounded-lg shadow-2xl "
+                    />
                 </div>
             </div>
         </div>
